f99a0e33cf93ed9b57ce4301dfcdafa4a413cbd4
Skyforgerr <postnikoviivan@gmail.com>
2023-03-05 18:54:05 +0300

    added main page controller

---
+++ src/main/java/com/skyforger/goods/controller/MainPageController.java
@@ -0,0 +1,12 @@
+package com.skyforger.goods.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Ivan 05.03.2023
+ */
+
+@Controller
+public class MainPageController {
+    
+}
