e10b38a144dd824a835b2bdb537f76a712001fdf
Skyforgerr <postnikoviivan@gmail.com>
2023-03-18 15:39:39 +0300

    добавил контроллер пользователя

--- src/main/java/com/skyforger/goods/service/UserService.java
+++ src/main/java/com/skyforger/goods/service/UserService.java
@@ -1,7 +1,10 @@
 package com.skyforger.goods.service;
 
+import com.skyforger.goods.model.User;
+
 /**
  * @author Ivan 18.03.2023
  */
 public interface UserService {
+    public User saveUser(User user);
 }
--- src/main/java/com/skyforger/goods/service/UserServiceImpl.java
+++ src/main/java/com/skyforger/goods/service/UserServiceImpl.java
@@ -1,7 +1,20 @@
 package com.skyforger.goods.service;
 
+import com.skyforger.goods.model.User;
+import com.skyforger.goods.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Ivan 18.03.2023
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
