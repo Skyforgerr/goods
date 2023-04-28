52c14618019875cff46bf017fb758d2125250cf9
Skyforgerr <postnikoviivan@gmail.com>
2023-04-28 08:33:31 +0300

    добавил файлы о товарах

---
+++ src/main/java/com/skyforger/goods/controller/GoodController.java
@@ -0,0 +1,21 @@
+// package com.skyforger.goods.controller;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import com.skyforger.goods.model.Good;
+// import org.springframework.web.bind.annotation.*;
+
+// import com.skyforger.goods.service.GoodService;
+
+// @RestController
+// @RequestMapping("/goods")
+// @CrossOrigin
+// public class GoodController {
+//     @Autowired
+//     private GoodService goodService;
+
+//     // @PostMapping("/add")
+//     // public String addGood(@ReduestBody Good good){
+//     //     goodService.saveGood(good);
+//     // }
+    
+// }
---
+++ src/main/java/com/skyforger/goods/service/GoodServiceImpl.java
@@ -0,0 +1,22 @@
+// package com.skyforger.goods.service;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import com.skyforger.goods.model.Good;
+
+// import com.skyforger.goods.repository.GoodRepository;
+// import java.util.List;
+
+// public class GoodServiceImpl implements GoodService{
+//     @Autowired
+//     private GoodRepository goodRepository;
+
+//     @Override
+//     public Good saveGood(Good good) {
+//         return goodRepository.save(good);
+//     }
+    
+//     @Override
+//     public List<Good> getAllGoods() {
+//         return goodRepository.findAll();
+//     }
+// }
